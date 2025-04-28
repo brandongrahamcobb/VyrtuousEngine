5049f770323b1bfc337d34be37147f979236881e
Brandon Graham Cobb <brandongrahamcobb@icloud.com>
2025-04-27 22:47:39 -0400

    28th fixes ChatListener.java

--- src/main/java/com/brandongcobb/vyrtuous/utils/listeners/ChatListener.java
+++ src/main/java/com/brandongcobb/vyrtuous/utils/listeners/ChatListener.java
@@ -23,7 +23,13 @@ import java.sql.Timestamp;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import org.anjocaido.groupmanager.GroupManager;
+import org.anjocaido.groupmanager.dataholder.OverloadedWorldHolder;
+import org.anjocaido.groupmanager.permissions.AnjoPermissionsHandler;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -32,69 +38,103 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 public class ChatListener implements Listener {
 
     private final Vyrtuous app;
+    private GroupManager groupManager;
     private final ExecutorService executor = Executors.newSingleThreadExecutor();
-
     private final PlayerMessageQueueManager chatQueuer;
+    volatile boolean continueInteraction;
 
     public ChatListener(Vyrtuous application, PlayerMessageQueueManager chatQueuer) {
         this.app = ConfigManager.getApp();
         this.chatQueuer = chatQueuer;
     }
 
+    public boolean hasGroupManager() {
+        if (groupManager != null) return true;
+            final PluginManager pluginManager = app.getServer().getPluginManager();
+            final Plugin GMplugin = pluginManager.getPlugin("GroupManager");
+            if (GMplugin != null && GMplugin.isEnabled()) {
+                groupManager = (GroupManager)GMplugin;
+                return true;
+            }
+            return false;
+    }
+
+    public static String removeStringIgnoreCase(String base, String remove) {
+        if (base == null || remove == null) {
+            return base;
+        }
+
+        int index = -1;
+        while ((index = base.toLowerCase().indexOf(remove.toLowerCase())) != -1) {
+            base = base.substring(0, index) + base.substring(index + remove.length());
+        }
+        return base;
+    }
+
+    public boolean hasPermission(final Player player, final String node) {
+        if (!hasGroupManager()) return false;
+        final AnjoPermissionsHandler handler = groupManager.getWorldsHolder().getWorldPermissions(player);
+        if (handler == null) return false;
+        return handler.has(player, node);
+    }
+
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
         String message = event.getMessage();
+        String decantedMessage = removeStringIgnoreCase(message, "vyrtuous");
         long playerId = player.getEntityId();
         if (chatQueuer.hasPlayer(playerId)) {
-            chatQueuer.enqueueMessage(playerId, message);
+            chatQueuer.enqueueMessage(playerId, decantedMessage);
             return;
         }
-        if (message.contains("Vyrtuous")) {
-            chatQueuer.addPlayer(playerId);
-            Bukkit.getScheduler().runTaskAsynchronously(app, () -> {
-                handleVyrtuousInteraction(player, message.replace("Vyrtuous", ""), playerId);
-            });
-        }
-    }
-
-    private void handleVyrtuousInteraction(Player player, String message, long playerId) {
-        boolean continueInteraction = true;
-        AIManager.handleConversation(playerId, message, null).thenAccept(result -> {
-            if (result.getValue()) {
-                return;
-            } else {
-                Bukkit.getScheduler().runTask(app, () -> {
-                    Bukkit.broadcastMessage("[Vyrtuous]: " + result.getKey());
-                });
-            }
-        });
-        while (continueInteraction) {
-            try {
-                String response = chatQueuer.dequeueMessage(playerId, 30, TimeUnit.SECONDS);
-                if (response == null) {
-                    break;
-                }
-                if (response.equalsIgnoreCase("STOP")) {
+        chatQueuer.addPlayer(playerId);
+        Bukkit.getScheduler().runTaskAsynchronously(app, () -> {
+            continueInteraction = true;
+            AIManager.handleConversation(playerId, decantedMessage, null).thenAccept(result -> {
+                if (result.getValue()) {
+                    event.setCancelled(true);
+                    player.sendMessage(result.getKey() + "." + ChatColor.RED + " Nobody saw your message.");
+                    continueInteraction = false;
+                    return;
+                } else if (hasPermission(player, "vyrtuous.openai") && message.toLowerCase().contains("vyrtuous")) {
                     Bukkit.getScheduler().runTask(app, () -> {
-                        player.sendMessage("[Vyrtuous] " + player.getName() + " stopped the interaction.");
+                        Bukkit.broadcastMessage(ChatColor.GREEN + "[Vyrtuous]: " + ChatColor.WHITE + result.getKey());
                     });
-                    break;
                 }
-                AIManager.handleConversation(playerId, response, null).thenAccept(result -> {
-                    if (result.getValue()) {
-                        return;
-                    } else {
+            });
+            while (continueInteraction) {
+                try {
+                    String response = chatQueuer.dequeueMessage(playerId, 30, TimeUnit.SECONDS);
+                    if (response == null) {
+                        break;
+                    }
+                    if (response.equalsIgnoreCase("STOP")) {
                         Bukkit.getScheduler().runTask(app, () -> {
-                            Bukkit.broadcastMessage("[Vyrtuous]: " + result.getKey());
+                            player.sendMessage(ChatColor.GREEN + "[Vyrtuous] " + ChatColor.WHITE + player.getName() + " stopped the interaction.");
                         });
-                   }
-                });
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                break;
+                        continueInteraction = false;
+                        break;
+                    }
+                    AIManager.handleConversation(playerId, response, null).thenAccept(result -> {
+                        if (result.getValue()) {
+                            event.setCancelled(true);
+                            player.sendMessage(result.getKey() + "." + ChatColor.RED + " Nobody saw your message.");
+                            continueInteraction = false;
+                            return;
+                        } else {
+                            Bukkit.getScheduler().runTask(app, () -> {
+                                Bukkit.broadcastMessage(ChatColor.GREEN + "[Vyrtuous]: " + ChatColor.WHITE + result.getKey());
+                            });
+                            return;
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    return;
+                }
             }
-        }
-        chatQueuer.removePlayer(playerId);
+            chatQueuer.removePlayer(playerId);
+        });
     }
 }
